efc391129e3223c1d6f029c59bd88417818e53be
codewithmurali <muralikumar.admire@gmail.com>
2023-01-03 21:18:48 +0530

    After count app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import "./App.css";
+import { useState } from "react";
+// import { Planets } from "./planets";
 
 function App() {
-  const planets = [
-    { name: "Mars", isGasPlanet: false },
-    { name: "Earth", isGasPlanet: false },
-    { name: "Jupitar", isGasPlanet: true },
-    { name: "Venus", isGasPlanet: false },
-    { name: "Neptune", isGasPlanet: true },
-    { name: "Uranus", isGasPlanet: true },
-  ];
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      {planets.map(
-        (planet, key) => planet.isGasPlanet && <h1>{planet.name}</h1>
-      )}
+    <div>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)}>Set to zero</button>
+      <br />
+      <label>{count}</label>
     </div>
   );
 }
 
 export default App;
+
+// const planets = [
+//   { name: "Mars", isGasPlanet: false },
+//   { name: "Earth", isGasPlanet: false },
+//   { name: "Jupitar", isGasPlanet: true },
+//   { name: "Venus", isGasPlanet: false },
+//   { name: "Neptune", isGasPlanet: true },
+//   { name: "Uranus", isGasPlanet: true },
+// ];
+// return (
+//   <div className="App">
+//     {planets.map(
+//       (planet, key) => planet.isGasPlanet && <h1>{planet.name}</h1>
+//     )}
+//   </div>
+// );
