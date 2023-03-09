885ab4367b46fe8304752f9f4ed1ae70647d3d31
codewithmurali <muralikumar.admire@gmail.com>
2023-03-09 20:43:57 +0530

    After completing Todo list

--- src/App.js
+++ src/App.js
@@ -1,60 +1,66 @@
-import "./App.css";
+import "./style.css";
 import { useState } from "react";
+import { Task } from "./Task";
 // import { Planets } from "./planets";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [input, changeInput] = useState("");
-  const [text, showHide] = useState(true);
-  const [textcolor, changeColor] = useState("black");
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
-  const decrement = () => {
-    setCount(count - 1);
-  };
-  const setZero = () => {
-    setCount(0);
-  };
-  const increment = () => {
-    setCount(count + 1);
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
   };
 
-  const handleInputEvent = (event) => {
-    changeInput(event.target.value);
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    };
+
+    setTodoList([...todoList, task]);
+  };
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
   };
 
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
   return (
     <div className="App">
-      <h1>{count}</h1>
-      <div>
-        <button onClick={decrement}>Decrement</button>
-        <button onClick={setZero}>Set Zero</button>
-        <button onClick={increment}>Increment</button>
+      <div className="addTask">
+        <input type="text" onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
       </div>
-      <input type="text" onChange={handleInputEvent} />
-      {text && (
-        <h1 style={{ color: textcolor }}>
-          {input} <br /> Click the button to hide me!!!
-        </h1>
-      )}
-      <button
-        onClick={() => {
-          showHide(!text);
-        }}
-      >
-        {" "}
-        Show / Hide{" "}
-      </button>
-      <h2 style={{ color: textcolor }}>Change the color</h2>
-      <input
-        type="color"
-        onChange={(event) => changeColor(event.target.value)}
-      />
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
 
 export default App;
 
+//_______Display Planets____________
 //
 // const planets = [
 //   { name: "Mars", isGasPlanet: false },
@@ -71,3 +77,55 @@ export default App;
 //     )}
 //   </div>
 // );
+//
+//
+//
+// ________Basics of use state____________
+//   const [count, setCount] = useState(0);
+//   const [input, changeInput] = useState("");
+//   const [text, showHide] = useState(true);
+//   const [textcolor, changeColor] = useState("black");
+
+//   const decrement = () => {
+//     setCount(count - 1);
+//   };
+//   const setZero = () => {
+//     setCount(0);
+//   };
+//   const increment = () => {
+//     setCount(count + 1);
+//   };
+
+//   const handleInputEvent = (event) => {
+//     changeInput(event.target.value);
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>{count}</h1>
+//       <div>
+//         <button onClick={decrement}>Decrement</button>
+//         <button onClick={setZero}>Set Zero</button>
+//         <button onClick={increment}>Increment</button>
+//       </div>
+//       <input type="text" onChange={handleInputEvent} />
+//       {text && (
+//         <h1 style={{ color: textcolor }}>
+//           {input} <br /> Click the button to hide me!!!
+//         </h1>
+//       )}
+//       <button
+//         onClick={() => {
+//           showHide(!text);
+//         }}
+//       >
+//         {" "}
+//         Show / Hide{" "}
+//       </button>
+//       <h2 style={{ color: textcolor }}>Change the color</h2>
+//       <input
+//         type="color"
+//         onChange={(event) => changeColor(event.target.value)}
+//       />
+//     </div>
+//   );
