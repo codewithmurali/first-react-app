7dd368bb911aab3adb1b43062a0ed6e39467d991
codewithmurali <muralikumar.admire@gmail.com>
2023-04-23 02:23:02 +0530

    After using Prop-types

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 export const Person = (props) => {
   return (
     <div>
-      <h1>Name: {props.namee}</h1>
+      <h1>Name: {props.name}</h1>
 
       <h1>Email: {props.email}</h1>
       <h1>Age: {props.age}</h1>
